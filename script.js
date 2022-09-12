6f23f06526fd74a9441a626817e121f7c759b8ad
stefan.forsberg <cykelkatalogen@popkram.com>
2022-09-12 17:26:18 +0200

    Updates

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const schedule = [
   [
     ["Pomodoro kyckling", "Kycklinglår,Fetaost,Morötter,PassTomater"],
-    ["Pytt i panna", "Veg pytt,Rödbeter,Ägg"],
-    ["Lax & mangosallad", "Lax,Ris,Lime,Mango (fryst)"],
     ["Järpar med bulgur", "Nötfärs,Riven cheddarost,Gurka,Matyoughurt,Bulgur"],
+    ["Lax & mangosallad", "Lax,Ris,Lime,Mango (fryst)"],
+    ["Pytt i panna", "Veg pytt,Rödbeter,Ägg"],
   ],
   [
-    ["Kyckling i kokosmjölk", "Kyckling,Nudlar,Paprika,Kokosmjölk,Currypasta", "https://www.ica.se/recept/kyckling-i-kokosmjolk-med-nudlar-714320/"],
+    ["Kyckling i kokosmjölk", "Kyckling,Nudlar,Paprika,Kokosmjölk,Currypasta,Purjolök", "https://www.ica.se/recept/kyckling-i-kokosmjolk-med-nudlar-714320/"],
     ["Köttbullar i tomatsås", "Nötfärs,Lök,Tomatsås,Pasta", "https://www.ica.se/recept/kottbullar-i-tomatsas-722787/"],
     ["Stekt lax, potatis & sås", "Lax,Potatis,Sås till laxen"],
     ["Korvstroganof", "Falukorv,Matyoghurt,Lök"],
